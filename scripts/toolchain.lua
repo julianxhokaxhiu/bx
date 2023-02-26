703ffd6242d476db42220053e495fdab140d95fa
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2023-02-26 15:52:05 +0100

    Revert "Bump SSE to 4.2"
    This reverts commit 340000754fe3fcf63c16841d228c33a461c335a3.

--- scripts/toolchain.lua
+++ scripts/toolchain.lua
@@ -522,9 +522,9 @@ function toolchain(_buildDir, _libDir)
 			"-Wno-tautological-constant-compare",
 		}
 
-	configuration { "vs*", "not NX32", "not NX64" }
+	configuration { "vs*", "x32" }
 		flags {
-			"EnableAVX",
+			"EnableSSE2",
 		}
 
 	configuration { "vs*", "not orbis", "not NX32", "not NX64" }
@@ -627,7 +627,7 @@ function toolchain(_buildDir, _libDir)
 			"-Wunused-value",
 			"-fdata-sections",
 			"-ffunction-sections",
-			"-msse4.2",
+			"-msse2",
 		}
 		linkoptions {
 			"-Wl,--gc-sections",
@@ -693,7 +693,7 @@ function toolchain(_buildDir, _libDir)
 
 	configuration { "linux-gcc* or linux-clang*" }
 		buildoptions {
-			"-msse4.2",
+			"-msse2",
 --			"-Wdouble-promotion",
 --			"-Wduplicated-branches",
 --			"-Wduplicated-cond",
@@ -833,7 +833,7 @@ function toolchain(_buildDir, _libDir)
 			"--target=i686-none-linux-android" .. androidApiLevel,
 			"-mtune=atom",
 			"-mstackrealign",
-			"-msse4.2",
+			"-msse3",
 			"-mfpmath=sse",
 		}
 		linkoptions {
@@ -935,7 +935,7 @@ function toolchain(_buildDir, _libDir)
 		}
 		buildoptions {
 			"-arch x86_64",
-			"-msse4.2",
+			"-msse2",
 			"-target x86_64-apple-macos" .. (#macosPlatform > 0 and macosPlatform or "13.0"),
 		}
 
